758cc39fd9576062db28eb00ac7de81f46bc4fd3
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-09-17 17:19:38 +0530

    Login and Registration Screen with validation and Print data of the Registration screen into next activity

---
+++ Tutorial05/app/src/main/java/com/rku/tutorial_05/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.rku.tutorial_05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText Username;
+    EditText Password;
+    Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Username = (EditText) findViewById(R.id.edt_username);
+        Password = (EditText) findViewById(R.id.pass_edt);
+        Login=(Button) findViewById(R.id.login_btn);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String valUsername=Username.getText().toString();
+                String valPassword=Password.getText().toString();
+
+                Log.i("Login Screen","In onClick");
+                if(!Patterns.EMAIL_ADDRESS.matcher(valUsername).matches())
+                {
+                    Toast.makeText(LoginActivity.this, "Email address format is not valid", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (valUsername.equals("admin@gmail.com") && valPassword.equals("admin")) {
+                    Log.i("Login Screen","In onClick If");
+
+                    Intent intent = new Intent(LoginActivity.this, Welcome_activity.class);
+                    intent.putExtra("userdata_value",valUsername);
+                    startActivity(intent);
+                    finish();
+
+                    Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Username or Password is incorrect.", Toast.LENGTH_SHORT).show();
+                    Log.i("Login Screen","In onClick else");
+                }
+            }
+        });
+    }
+
+    public void Registerscreen(View view) {
+        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
