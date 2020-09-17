146c9e8cbd5eba0ae2c3ef269eb54a04bd789f9c
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-09-17 17:20:56 +0530

    Login & Logout with SharedPreference

---
+++ Tutorial_06/app/src/main/java/com/rku/tutorial_06/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.rku.tutorial_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
