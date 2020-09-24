85bf75e24acb24adf673a40518d7df4c126cab0f
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-09-24 12:37:45 +0530

    Tutorial with ScrollView and Landscape Mode

---
+++ Tutorial_02/app/src/main/java/com/rku/tutorial_02/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rku.tutorial_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(getResources().getString(R.string.about_us));
+    }
+}
