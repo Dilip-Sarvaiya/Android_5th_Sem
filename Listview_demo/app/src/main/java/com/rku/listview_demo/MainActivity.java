2aec5c70c02614f00072571b7742d91cc80cadf4
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-09-29 17:38:59 +0530

    This is the Demo for Inbuilt Listview and Custom ListView

---
+++ Listview_demo/app/src/main/java/com/rku/listview_demo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.rku.listview_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String [] data={"Apple","Banana","Mango"};
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.lstview);
+
+        adapter=new ArrayAdapter<String>(MainActivity.this,R.layout.list_view_template,data);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,((TextView)view).getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
